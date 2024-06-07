3148ffb60e1475ade45b95e2f83f6f3ee94f664a
choocol <lukacosta340@gmail.com>
2024-06-07 16:38:43 -0300

    aula

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("aula de git");
+class carro{
+    constructor(tipo,capacidade){
+      this.tipo = tipo
+      this.capacidade = capacidade
+    }
+}
+
+const gol = new carro("carro",5);
