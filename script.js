d0fb9624c9586c33ca6ec4afb0c3b4a2886bd6ee
choocol <lukacosta340@gmail.com>
2024-06-07 17:30:39 -0300

    classe animal adicionada

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("branch suporte");
+console.log("aula de git");
+class carro{
+    constructor(tipo,capacidade){
+      this.tipo = tipo
+      this.capacidade = capacidade
+    }
+}
+
+const gol = new carro("carro",5);
+
+class animal{
+    constructor(tipo,patas){
+    this.tipo = tipo;
+    this.patas = patas;
+  }
+}
+
+const cachorro = new animal("mamifero",4);
